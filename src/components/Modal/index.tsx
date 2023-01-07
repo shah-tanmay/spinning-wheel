20e2b64bbf80fa6a7d71537c463a1478e254e04d
Tanmay <shahhtanmay@gmail.com>
2023-01-07 21:13:35 +0530

    building

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,6 +8,8 @@ const customStyles = {
     bottom: "auto",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
+    width: "400px",
+    // height: "300px",
   },
 };
 
@@ -22,10 +24,10 @@ const CustomModal = ({
 }) => {
   return (
     <Modal isOpen={isOpen} onRequestClose={closeModal} style={customStyles}>
-      <h1 style={{ display: "flex", justifyContent: "center", width: "100%" }}>
+      <h1 style={{ display: "flex", justifyContent: "center" }}>
         Congratulations 🎉🥳🎊!
       </h1>
-      <h2 style={{ display: "flex", justifyContent: "center", width: "100%" }}>
+      <h2 style={{ display: "flex", justifyContent: "center" }}>
         You have won ₹{price}
       </h2>
     </Modal>
